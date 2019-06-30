7a295f14915b01b6efde41f88f5a87666bfe6bda
Irfan Sheikh <irfan.sh3112@gmail.com>
2019-07-01 00:56:41 +0530

    added local path functionality in clone-all-repos

--- clone-all-repos.sh
+++ clone-all-repos.sh
@@ -3,6 +3,7 @@
 BASE_URL='https://api.github.com/user/repos'
 
 read -p 'Authorization token: ' AUTH_TOKEN
-
+read -p 'Enter local path where the repositories should be cloned (. for current working directory): ' LOCAL_PATH
+cd $LOCAL_PATH
 curl -i -H "Authorization: token $AUTH_TOKEN" $BASE_URL | grep ssh_url | cut -d '"' -f 4 | xargs -L1 git clone
 
